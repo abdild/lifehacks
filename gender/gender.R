6efd464c38bccecc9c5f0bcd5f8e1258a968d4de
aii <abdullintat@yandex.ru>
2020-03-22 22:43:42 +0300

    add files

--- gender/gender.R
+++ gender/gender.R
@@ -1,30 +1,22 @@
-library(readxl) # Чтение excel файлов
-library(tidyverse) # 
+# Подключаем библиотеки
+library(readxl) # Чтение файла *.xlsx
+library(tidyverse)
+library(writexl) # Запись файла *.xlsx
 
 # Считываем файл данных
-data_base = read_xlsx("https://github.com/abdild/lifehacks/blob/master/%D0%9F%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D0%BA%D0%B0%20%D0%BF%D0%BE%D0%BB%D0%B0/fio.xlsx")
-data_base = import("ФИО.xlsx", sheet = 2)
+data_base = read_xlsx("fio_base.xlsx")
 
-# Добавляем столбец с полом (gender)
+# Добавляем столбец с полом
 ds =  mutate(
   data_base,
-  gender = case_when(
-    str_sub(data_base$Сотрудник, -4, -1) == "ович" ~ "м",
-    str_sub(data_base$Сотрудник, -4, -1) == "евич" ~ "м",
-    str_sub(data_base$Сотрудник, -4, -1) == "оглы" ~ "м",
-    str_sub(data_base$Сотрудник, -4, -1) == "овна" ~ "ж",
-    str_sub(data_base$Сотрудник, -4, -1) == "евна" ~ "ж",
-    str_sub(data_base$Сотрудник, -4, -1) == "кызы" ~ "ж",
-    str_sub(data_base$Сотрудник, -4, -1) == "ична" ~ "ж",
+  `Пол` = case_when(
+    str_sub(data_base$ФИО, -3, -1) == "вич" ~ "м",
+    str_sub(data_base$ФИО, -3, -1) == "вна" ~ "ж",
     TRUE ~ "!"
     )
   )
 
-# Проверка все ли строки обработались
- ds %>% filter(ds$gender == "!")
+# Записываем файл
+write_xlsx(ds, "fio_R.xlsx")
 
-# Группировка по полу
- ds %>%
-   group_by(gender) %>%
-   summarise(total = n())
  
